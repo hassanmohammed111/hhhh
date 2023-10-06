53cfa029c614229afdfb0a2f810ec3dd6b69147b
Doniasoliman <solimandonia35@gmail.com>
2023-10-06 19:03:49 +0300

    first commit

---
+++ module.js
@@ -0,0 +1,16 @@
+// const amount = 12
+
+// if(amount < 10) {
+//    console.log('small number');
+// } else {
+//    console.log('large number');
+// }
+// console.log('ydfrdt');
+
+const names = require('./names')
+// const SayHi = require('./utils')
+const data = require('./alternative-flaver')
+require('./mind-grande')
+// SayHi('mariam') 
+// SayHi(names.ahmed) 
+// SayHi(names.mohamed) 
---
+++ names.js
@@ -0,0 +1,5 @@
+const secret = 'Super secret'
+const ahmed = 'ahmed'
+const mohamed = 'mohamed'
+
+module.exports = { ahmed,mohamed }
